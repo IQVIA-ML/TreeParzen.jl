4ea9ed00cfb4c3a73641e21e8dd0910adc90e742
Mubeena Rasack <mrasack@users.noreply.github.com>
2020-05-20 11:41:40 +0530

    changed logquant test

--- test/logquantuniform.jl
+++ test/logquantuniform.jl
@@ -18,12 +18,10 @@ using TreeParzen
     @test 1.0 <= minimum(qlu_samples)
     @test maximum(qlu_samples) <= 100.0
 
-
-    # get their max and min so we don't really need to check against a set
     sample_vals = sort(unique(qlu_samples))
-
     samples = log.(sample_vals)
     gap = round.(diff(samples); digits=1)
+    
     @test length(unique(gap)) == 1
     @test unique(gap)[1] == q
 end
