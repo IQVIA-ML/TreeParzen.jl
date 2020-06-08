e46ef33e4d2bd0d8ab7a19113aee5edb133411de
yalwan-iqvia <yaqub.alwan@iqvia.com>
2020-06-08 11:49:34 +0100

    String Support, v2 (#34)

--- test/spaces.jl
+++ test/spaces.jl
@@ -9,10 +9,26 @@ space = Dict(
     :b => HP.Choice(:b, ["a", "b", "c"])
 )
 
-sample = ask(space).hyperparams
+# just asking, isn't good enough
+trial = ask(space)
+sample = trial.hyperparams
 
 @test sample[:a] in (:a, :b, :c)
 @test sample[:b] in ("a", "b", "c")
+
+# gotta ask both ways
+trials = TreeParzen.Trials.Trial[]
+tell!(trials, trial, 2.)
+trial = ask(space)
+tell!(trials, trial, 2.6)
+
+tp_trial = ask(space, trials, TreeParzen.Config())
+tp_sample = tp_trial.hyperparams
+
+# same holds true but we have to make sure we can sample from strings literals via TP asks
+@test tp_sample[:a] in (:a, :b, :c)
+@test tp_sample[:b] in ("a", "b", "c")
+
 
 end
 true
