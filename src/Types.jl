ee5fbf2294e8269fedaf2ddc95b36601c80d1827
Yaqub Alwan <yaqub.alwan@iqvia.com>
2020-11-17 09:57:33 +0000

    this could get hairy

--- src/Types.jl
+++ src/Types.jl
@@ -2,6 +2,6 @@ module Types
 
 abstract type AbstractDelayed end
 
-const SPACE_TYPE = Union{Dict{Symbol, T} where T, AbstractDelayed, AbstractVector{<: AbstractDelayed}}
+const SPACE_TYPE = Union{Dict{Symbol, T} where T, AbstractDelayed, AbstractVector{<: AbstractDelayed}, AbstractVector{Dict{Symbol, T} where T}}
 
 end # module
