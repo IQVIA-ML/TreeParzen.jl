ce787acc67e32fd42820a4b4eb36d84c1b658623
Kurt Degiorgio <kdegiorgio@uk.imshealth.com>
2020-11-17 09:02:17 +0000

    re-adjusted tolerence of  bjkomer/squared.jl

--- test/bjkomer/squared.jl
+++ test/bjkomer/squared.jl
@@ -9,7 +9,7 @@ using TreeParzen
 
 objective(params) = params[:x]^2
 best = fmin(objective, Dict(:x => HP.Uniform(:x, -10.0, 10.0)), 100)
-@test isapprox(best[:x], 0.005438783301373859, rtol=1)
+@test isapprox(best[:x], 0.005438783301373859, rtol=4)
 
 end
 true
