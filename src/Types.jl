0a483fdeaf30feec6f668f80ee25b8c371e5412d
yalwan-iqvia <yaqub.alwan@iqvia.com>
2020-11-17 10:08:05 +0000

    Update Types.jl

--- src/Types.jl
+++ src/Types.jl
@@ -2,6 +2,11 @@ module Types
 
 abstract type AbstractDelayed end
 
-const SPACE_TYPE = Union{Dict{Symbol, T} where T, AbstractDelayed, AbstractVector{<: AbstractDelayed}, AbstractVector{Dict{Symbol, T} where T}}
+const SPACE_TYPE = Union{
+    Dict{Symbol, T} where T,
+    AbstractDelayed,
+    AbstractVector{<: AbstractDelayed},
+    AbstractVector{Dict{Symbol, T} where T},
+}
 
 end # module
