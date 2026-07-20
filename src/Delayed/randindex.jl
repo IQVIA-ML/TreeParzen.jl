7edf1892526ccafeafb7978dcfebb6273f8552e7
kainkad <59925069+kainkad@users.noreply.github.com>
2026-07-20 09:38:38 +0100

    refactor based on comment

--- src/Delayed/randindex.jl
+++ src/Delayed/randindex.jl
@@ -5,15 +5,13 @@ $(TYPEDFIELDS)
 """
 struct RandIndex{U} <: AbstractDistDelayed
     upper::U
-end
 
-function RandIndex(upper::Types.AbstractDelayed)
-    return RandIndex{typeof(upper)}(upper)
-end
-function RandIndex(upper::Int)
-    if upper < 1
-        throw(ArgumentError("upper will be used as index so must be greater than 0"))
+    RandIndex(upper::Types.AbstractDelayed) = new{typeof(upper)}(upper)
+    function RandIndex(upper::Int)
+        if upper < 1
+            throw(ArgumentError("upper will be used as index so must be greater than 0"))
+        end
+
+        return new{Int}(upper)
     end
-
-    return RandIndex{Int}(upper)
 end
